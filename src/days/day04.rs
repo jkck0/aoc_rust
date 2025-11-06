4c6e0f68b15f7b9ce304bbb6eb1214ffce189759
jkck0 <160541598+jkck0@users.noreply.github.com>
2025-11-06 14:38:01 -0500

    added multithreading to day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,31 +1,89 @@
+use std::{
+    sync::{
+        Arc,
+        atomic::{AtomicU32, Ordering},
+    },
+    thread,
+};
+
 use md5;
 
 pub fn parse(data: &str) -> &str {
     data
 }
 
-fn count_zeroes(hash: &str) -> usize {
-    hash.chars().take_while(|c| *c == '0').count()
+fn count_zeroes(hash: &str) -> u32 {
+    hash.chars().take_while(|c| *c == '0').count() as u32
 }
 
-fn find_hash(key: &str, zeroes: usize) -> u32 {
-    let mut x = 1;
-    loop {
-        let hash = format!("{:x}", md5::compute(key.to_owned() + &x.to_string()));
-        if count_zeroes(&hash) >= zeroes {
-            break;
-        }
+struct State {
+    key: String,
+    zeroes: u32,
+    chunk_size: u32,
 
-        x += 1;
+    next_chunk: AtomicU32,
+    answer: AtomicU32,
+}
+
+impl State {
+    fn new(key: String, zeroes: u32, chunk_size: u32) -> Self {
+        State {
+            key: key,
+            zeroes: zeroes,
+            chunk_size: chunk_size,
+            next_chunk: AtomicU32::new(0),
+            answer: AtomicU32::new(0),
+        }
     }
 
-    x
+    // returns the next chunk to search, until we find an answer
+    fn next_chunk(&self) -> Option<u32> {
+        if self.answer.load(Ordering::Relaxed) == 0 {
+            Some(
+                self.next_chunk
+                    .fetch_add(self.chunk_size, Ordering::Relaxed),
+            )
+        } else {
+            None
+        }
+    }
+}
+
+fn find_hash(state: State) -> u32 {
+    let num_threads = thread::available_parallelism().unwrap().get();
+    let state_arc = Arc::new(state);
+
+    let mut handles = Vec::with_capacity(num_threads);
+    for _ in 0..num_threads {
+        let state_clone = state_arc.clone();
+        handles.push(thread::spawn(move || {
+            worker(state_clone);
+        }));
+    }
+
+    for handle in handles {
+        _ = handle.join();
+    }
+    state_arc.answer.load(Ordering::Relaxed)
+}
+
+fn worker(state: Arc<State>) {
+    while let Some(chunk) = state.next_chunk() {
+        for x in chunk..chunk + state.chunk_size {
+            let hash = format!("{:x}", md5::compute(state.key.clone() + &x.to_string()));
+            if count_zeroes(&hash) >= state.zeroes {
+                state.answer.store(x, Ordering::Relaxed);
+            }
+        }
+    }
 }
 
 pub fn part1(input: &str) -> u32 {
-    find_hash(input, 5)
+    let state = State::new(input.to_owned(), 5, 1000);
+    find_hash(state)
 }
 
 pub fn part2(input: &str) -> u32 {
-    find_hash(input, 6)
+    let state = State::new(input.to_owned(), 6, 1000);
+    find_hash(state)
 }
